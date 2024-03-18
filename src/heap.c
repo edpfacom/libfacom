f06e94c18779d34a9955378988dd3da03af1b8b7
edpfacom <edpfacom@gmail.com>
2024-03-18 11:09:41 -0400

    constroi heap added

--- src/heap.c
+++ src/heap.c
@@ -2,6 +2,13 @@
 #include<stdlib.h>
 #include<assert.h>
 
+typedef struct{
+    void ** v;
+    int tam;
+    void  (*cmp)(void *, void *);
+}theap;
+
+
 int pai(int n){
     return (n-1)/2;
 }
@@ -38,19 +45,49 @@ void constroi_heap(int v[], int max_size){
 }
 
 void sobe(int v[], int pos){
-    int p = pai(pos);
+    while(v[pai(pos)] < v[pos]){
+        troca(&v[pai(pos)],&v[pos]);
+        pos = pai(pos);
+    }
 }
 
 int acessa_max(int v[]){
+    return v[0];
 }
 
 int extrai_max(int v[], int * tam){
+    int ret = v[0];
+    *tam = *tam -1;
+    v[0] = v[*tam];
+    desce(v,0,*tam);
+    return ret;
 }
 int insere_elemento(int v[], int *tam, int max, int novo){
+    int ret = EXIT_FAILURE;
+    if (*tam <= max){
+        v[*tam] = novo;
+        sobe(v,*tam);
+        *tam += 1;
+        ret = EXIT_SUCCESS;
+    }
+    return ret;
 }
 int altera_prioridade(int v[],int tam, int pos, int novo){
+    if (v[pos] < novo){ /*sobe*/
+        v[pos] = novo;
+        sobe(v,pos);
+    }else if (v[pos] > novo){ /*desce*/
+        v[pos] = novo;
+        desce(v,pos,tam);
+    }
+    return EXIT_SUCCESS;
 }
 void heapsort(int v[], int tam){
+    int i;
+    for (i = tam-1;i>=0;i--){
+        troca(&v[0],&v[i]);
+        desce(v,0,i);
+    }
 }
 void test_pai(){
     /*inicializacao */
