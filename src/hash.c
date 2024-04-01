b45984a5754d8f5844c50d166f25da5cec4b632c
edpfacom <edpfacom@gmail.com>
2024-04-01 10:38:21 -0400

    construtor implementado fix #2

--- src/hash.c
+++ src/hash.c
@@ -19,14 +19,23 @@ uint32_t hashf(const char* str, uint32_t h){
 
 
 int hash_insere(thash * h, void * bucket){
+    uint32_t hash = hashf(h->get_key(bucket),SEED);
+    int pos = hash %(h->max);
     return EXIT_SUCCESS;
 }
 
 
 
 int hash_constroi(thash * h,int nbuckets, char * (*get_key)(void *) ){
+    h->table = calloc(sizeof(void *),nbuckets + 1);
+    if (h->table == NULL){
+        return EXIT_FAILURE;
+    }
+    h->max = nbuckets + 1;
+    h->size = 0;
+    h->deleted = (uintptr_t)&(h->size);
+    h->get_key = get_key;
     return EXIT_SUCCESS;
-
 }
 
 
