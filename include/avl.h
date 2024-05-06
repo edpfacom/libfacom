430d811795da4cff2c5bbdf95fa89c58faa0ca45
edpfacom <128524511+edpfacom@users.noreply.github.com>
2024-05-06 09:50:45 -0400

    Create avl.h

---
+++ include/avl.h
@@ -0,0 +1,17 @@
+#ifndef __AVL__
+#define  __AVL__
+
+typedef int titem;
+
+typedef struct _node{
+    titem item;
+    struct _node *esq;
+    struct _node *dir;
+    int h;
+}tnode;
+
+void avl_insere(tnode ** parv,titem reg);
+void avl_remove(tnode ** parv,titem reg);
+
+
+#endif
